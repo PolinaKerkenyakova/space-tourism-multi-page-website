14846a7f8448c5f7cc599b1312e5cb496f5929c4
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-21 20:24:53 +0200

    Add primary header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import LargeButton from './components/NavigationButtons/LargeButton';
+import LargeButton from './components/Navigation/NavigationButtons/LargeButton';
 import PrimaryNavigation from './components/Navigation/PrimaryNavigation';
 import TabNavigation from './components/Navigation/TabNavigation';
 import DotNavigation from './components/Navigation/DotNavigation';
 import NumberNavigation from './components/Navigation/NumberNavigation';
+import PrimaryHeader from './components/Header/PrimaryHeader';
 
 function App() {
   return (
     <>
-      <LargeButton classes={['uppercase', 'ff-serif', 'fs-600', 'text-dark', 'bg-white', 'uppercase']} />
+      <PrimaryHeader />
+      {/* <LargeButton classes={['uppercase', 'ff-serif', 'fs-600', 'text-dark', 'bg-white', 'uppercase']} />
       <PrimaryNavigation />
       <TabNavigation />
       <DotNavigation />
-      <NumberNavigation />
+      <NumberNavigation /> */}
     </>
   );
 }
---
+++ src/components/Header/PrimaryHeader.js
@@ -0,0 +1,13 @@
+import PrimaryNavigation from '../Navigation/PrimaryNavigation';
+import './PrimaryHeader.css';
+
+const PrimaryHeader = () => {
+    return (
+        <header>
+            <div></div>
+            <PrimaryNavigation />
+        </header>
+    )
+}
+
+export default PrimaryHeader;
