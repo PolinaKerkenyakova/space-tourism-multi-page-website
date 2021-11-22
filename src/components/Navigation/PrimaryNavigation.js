616645d91adadb086c27b2289ac3ae8fb79de445
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-22 10:53:40 +0200

    Add font style

--- src/components/Navigation/PrimaryNavigation.js
+++ src/components/Navigation/PrimaryNavigation.js
@@ -6,10 +6,10 @@ const PrimaryNavigation = () => {
     return (
         <nav>
             <ul className="primary-navigation underline-indicators flex">
-                <li className="active"><Link to="/" className="uppercase letter-spacing-2 text-white"><span>00</span>Home</Link></li>
-                <li><Link to="/destinations" className="uppercase text-white letter-spacing-2"><span>01</span>Destinations</Link></li>
-                <li><Link to="/crew" className="uppercase text-white letter-spacing-2"><span>02</span>Crew</Link></li>
-                <li><Link to="/technology" className="uppercase text-white letter-spacing-2"><span>03</span>Technology</Link></li>
+                <li className="active"><Link to="/" className="ff-sans-cond uppercase letter-spacing-2 text-white"><span>00</span>Home</Link></li>
+                <li><Link to="/destinations" className="ff-sans-cond uppercase text-white letter-spacing-2"><span>01</span>Destinations</Link></li>
+                <li><Link to="/crew" className="ff-sans-cond uppercase text-white letter-spacing-2"><span>02</span>Crew</Link></li>
+                <li><Link to="/technology" className="ff-sans-cond uppercase text-white letter-spacing-2"><span>03</span>Technology</Link></li>
             </ul>
         </nav>
     )    
