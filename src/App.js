6322597cc6e7f64e52959062af581f049aed8ae5
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-22 11:11:10 +0200

    Add markup for home page

--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import Crew from "./components/Crew/Crew";
 import Destinations from "./components/Destinations/Destinations";
-import Header from './components/Header/Header';
+// import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Technology from "./components/Technology/Technology";
 
 function App() {
   return (
-    <div className="site-wrapper">
-
-      <Header />
-      <main>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/destinations' element={<Destinations />} />
-          <Route path='/crew' element={<Crew />} />
-          <Route path='/technology' element={<Technology />} />
-        </Routes>
-      </main>
-    </div>
+    < main >
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/destinations' element={<Destinations />} />
+        <Route path='/crew' element={<Crew />} />
+        <Route path='/technology' element={<Technology />} />
+      </Routes>
+    </main >
   );
 }
 
