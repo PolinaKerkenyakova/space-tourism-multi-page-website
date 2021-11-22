74a782ab8cbab93b7e2a652e1088d2961a8925d0
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-22 13:42:23 +0200

    Add home page html and styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,9 +6,9 @@ const Home = () => {
     return (
         <div className="home">
             <Header />
-            <div className="grid-container">
+            <div className="grid-container grid-container--home">
                 <div>
-                    <h1 className="text-light fs-500 ff-sans-cond uppercase letter-spacing-1">So, you want to travel to <span className="fs-900 ff-serif text-white">space</span></h1>
+                    <h1 className="text-light fs-500 ff-sans-cond uppercase letter-spacing-1">So, you want to travel to <span className="d-block fs-900 ff-serif text-white">space</span></h1>
                     <p>
                         Let’s face it; if you want to go to space, you might as well genuinely go to
                         outer space and not hover kind of on the edge of it. Well sit back, and relax
@@ -16,7 +16,7 @@ const Home = () => {
                     </p>
                 </div>
                 <div>
-                    <LargeButton classes={['uppercase', 'ff-serif', 'fs-600', 'text-dark', 'bg-white']} />
+                    <LargeButton classes={['uppercase', 'ff-serif', 'text-dark', 'bg-white']} />
                 </div>
             </div>
         </div>
