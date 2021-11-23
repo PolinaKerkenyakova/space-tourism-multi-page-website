033ce158c44ea6bbd9450a16ee945d30bb2b1484
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-23 14:12:06 +0200

    Add mobile, tablet and desktop responsive navigation

--- src/components/Navigation/PrimaryNavigation.js
+++ src/components/Navigation/PrimaryNavigation.js
@@ -15,11 +15,11 @@ const PrimaryNavigation = () => {
         <>
             <button onClick={showMenuHandler} className="mobile-nav-toggle" aria-controls="primary-navigation"><span className="sr-only" aria-expanded="true">Menu</span><img src={closeMenu} alt="" /></button>
             <nav>
-                <ul id="primary-navigation" className="primary-navigation underline-indicators flex">
-                    <li><Link to="/" className="ff-sans-cond uppercase letter-spacing-2 text-white"><span>00</span>Home</Link></li>
-                    <li><Link to="/destinations" className="ff-sans-cond uppercase text-white letter-spacing-2"><span>01</span>Destinations</Link></li>
-                    <li><Link to="/crew" className="ff-sans-cond uppercase text-white letter-spacing-2"><span>02</span>Crew</Link></li>
-                    <li><Link to="/technology" className="ff-sans-cond uppercase text-white letter-spacing-2"><span>03</span>Technology</Link></li>
+                <ul id="primary-navigation" data-visible="true" className="primary-navigation underline-indicators flex">
+                    <li><Link to="/" className="ff-sans-cond uppercase letter-spacing-2 text-white">< span aria-hidden="true">00</span>Home</Link></li>
+                    <li><Link to="/destinations" className="ff-sans-cond uppercase text-white letter-spacing-2"><span aria-hidden="true">01</span>Destinations</Link></li>
+                    <li><Link to="/crew" className="ff-sans-cond uppercase text-white letter-spacing-2"><span aria-hidden="true">02</span>Crew</Link></li>
+                    <li><Link to="/technology" className="ff-sans-cond uppercase text-white letter-spacing-2"><span aria-hidden="true">03</span>Technology</Link></li>
                 </ul>
             </nav>
         </>
@@ -29,12 +29,14 @@ const PrimaryNavigation = () => {
         <>
             <button onClick={showMenuHandler} className="mobile-nav-toggle" aria-controls="primary-navigation"><span className="sr-only" aria-expanded="false">Menu</span><img src={openMenu} alt="" /></button>
             <nav>
+                <ul data-visible="false" className="primary-navigation"></ul>
             </nav>
         </>
     )
 
     return (
-        isMenuVisible ? ShowMenu : HideMenu
+        // isMenuVisible ? ShowMenu : HideMenu
+        ShowMenu
     );
 }
 
