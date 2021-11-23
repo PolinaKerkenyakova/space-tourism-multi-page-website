e5f75d9fb5424b08a241a4515cc6b12f296a8499
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-23 14:07:27 +0200

    Add additional button

--- src/components/Navigation/TabNavigation.js
+++ src/components/Navigation/TabNavigation.js
@@ -6,6 +6,7 @@ const TabNavigation = () => {
             <button aria-selected="true" className="bg-dark text-white uppercase letter-spacing-3 ff-sans-cond">Moon</button>
             <button aria-selected="false" className="bg-dark text-white uppercase letter-spacing-3 ff-sans-cond">Mars</button>
             <button aria-selected="false" className="bg-dark text-white uppercase letter-spacing-3 ff-sans-cond">Europa</button>
+            <button aria-selected="false" className="bg-dark text-white uppercase letter-spacing-3 ff-sans-cond">Titan</button>
         </div>
     )
 }
