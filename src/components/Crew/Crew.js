1a2ed69c7fe5073501c054c6acec21f88a1adb49
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-24 11:17:39 +0200

    Add crew markup and mobile styling

--- src/components/Crew/Crew.js
+++ src/components/Crew/Crew.js
@@ -1,10 +1,33 @@
 import Header from "../Header/Header";
+import DotNavigation from "../Navigation/DotNavigation";
+
+import './Crew.css';
 
 const Crew = () => {
     return (
         <div className="crew">
             <Header />
-            <h1>Crew</h1>
+            <main id="main" className="grid-container grid-container--crew flow">
+                <h1 className="numbered-title"><span aria-hidden="true">02</span> Meet your crew</h1>
+
+                <div className="img">
+
+                </div>
+                <img src="/assets/crew/image-douglas-hurley.png" alt="" />
+
+                <DotNavigation />
+
+                <article className="crew-info flow">
+                    <h2 className="ff-serif uppercase fs-600 text-white">Commander</h2>
+                    <p className="ff-serif uppercase fs-700">Douglas Hurley</p>
+                    <p className="text-light fs-400">
+                        Douglas Gerald Hurley is an American engineer, former Marine Corps pilot
+                        and former NASA astronaut. He launched into space for the third time as
+                        commander of Crew Dragon Demo-2.
+                    </p>
+                </article>
+
+            </main>
         </div>
     )
 }
