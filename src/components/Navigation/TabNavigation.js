d1cad69f1af8b1f7e1fd7dfd08adda5aad5f4f46
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-24 14:16:04 +0200

    Remove buttons background

--- src/components/Navigation/TabNavigation.js
+++ src/components/Navigation/TabNavigation.js
@@ -3,10 +3,10 @@ import './TabNavigation.css';
 const TabNavigation = () => {
     return (
         <div className="flex tab-navigation underline-indicators">
-            <button aria-selected="true" className="bg-dark text-white uppercase letter-spacing-3 ff-sans-cond">Moon</button>
-            <button aria-selected="false" className="bg-dark text-white uppercase letter-spacing-3 ff-sans-cond">Mars</button>
-            <button aria-selected="false" className="bg-dark text-white uppercase letter-spacing-3 ff-sans-cond">Europa</button>
-            <button aria-selected="false" className="bg-dark text-white uppercase letter-spacing-3 ff-sans-cond">Titan</button>
+            <button aria-selected="true" className="text-white uppercase letter-spacing-3 ff-sans-cond">Moon</button>
+            <button aria-selected="false" className="text-white uppercase letter-spacing-3 ff-sans-cond">Mars</button>
+            <button aria-selected="false" className="text-white uppercase letter-spacing-3 ff-sans-cond">Europa</button>
+            <button aria-selected="false" className="text-white uppercase letter-spacing-3 ff-sans-cond">Titan</button>
         </div>
     )
 }
