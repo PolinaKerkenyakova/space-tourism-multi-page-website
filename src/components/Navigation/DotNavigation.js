68eaaeff6302e52053618c0e5b34df46849508ae
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-24 11:17:22 +0200

    Add one more button

--- src/components/Navigation/DotNavigation.js
+++ src/components/Navigation/DotNavigation.js
@@ -3,9 +3,10 @@ import './DotNavigation.css';
 const DotNavigation = () => {
     return (
         <div className="flex dot-indicators">
-            <button aria-selected="true"><span className="sr-only">Slide Title</span></button>
-            <button aria-selected="false"><span className="sr-only">Slide Title</span></button>
-            <button aria-selected="false"><span className="sr-only">Slide Title</span></button>
+            <button aria-selected="true"><span className="sr-only">Commander</span></button>
+            <button aria-selected="false"><span className="sr-only">Mission Specialist</span></button>
+            <button aria-selected="false"><span className="sr-only">Pilot</span></button>
+            <button aria-selected="false"><span className="sr-only">Flight Engineer</span></button>
         </div>
     )
 }
