37a9e7104f304efa1ae7458aa9bf01ddad937c8e
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-25 19:17:55 +0200

    Add functional technology data fetching and rendering

--- src/components/Technology/Technology.js
+++ src/components/Technology/Technology.js
@@ -1,33 +1,53 @@
-import Header from "../Header/Header";
-import NumberNavigation from "../Navigation/NumberNavigation";
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import Header from '../Header/Header';
+import NumberNavigation from '../Navigation/NumberNavigation';
+
+import getData from '../../services/fetchServices';
 
 import './Technology.css';
 
 const Technology = () => {
+
+    const [techInfo, setTechInfo] = useState({});
+    const [selectedTech, setSelectedTech] = useState();
+
+    const getSelectedTechHandler = (techId) => {
+        setSelectedTech(techId);
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const techData = await getData('technology', selectedTech);
+            setTechInfo(techData);
+        }
+
+        fetchData();
+    }, [selectedTech]);
+
     return (
         <div className="technology">
-            <a href="#main" className="skip-to-content">Skip to content</a>
+            <Link to="#main" className="skip-to-content">Skip to content</Link>
             <Header />
             <main id="main" className=" grid-container grid-container--technology flow">
                 <h1 className="numbered-title"><span aria-hidden="true">03</span> Space launch 101</h1>
 
-                <NumberNavigation />
+                <NumberNavigation getSelectedTech={getSelectedTechHandler} />
                 <article className="terminology-info">
                     <div className="flow flow-space--small ">
                         <h2 className="ff-sans-cond letter-spacing-3 fs-200 text-light uppercase">The terminology...</h2>
-                        <p className="ff-serif fs-700 uppercase">Launch vehicle</p>
+                        <p className="ff-serif fs-700 uppercase">{techInfo.name}</p>
                         <p className="text-light">
-                            A launch vehicle or carrier rocket is a rocket-propelled vehicle used to  payload from Earth's surface to space, usually to Earth orbit or beyond. Our
-                            WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall,
-                            it's quite an awe-inspiring sight on the launch pad!
+                            {techInfo.description}
                         </p>
                     </div>
                 </article>
 
                 <picture>
-                    <source media="(max-width: 719px)" srcSet="/assets/technology/image-launch-vehicle-landscape.jpg" type="image/jpg" />
-                    <source media="(min-width: 720px)" srcSet="/assets/technology/image-launch-vehicle-portrait.jpg" type="image/jpg" />
-                    <img src="/assets/technology/image-launch-vehicle-portrait.jpg" alt="" />
+                    <source media="(max-width: 720px)" srcSet={techInfo.landscapeImg} type="image/jpg" />
+                    <source media="(min-width: 720px)" srcSet={techInfo.portraitImg} type="image/jpg" />
+                    <img src="/assets/technology/image-launch-vehicle-portrait.jpg" alt={techInfo.name} />
                 </picture>
             </main>
         </div>
